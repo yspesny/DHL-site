881cd9ba14de571ff377f98e95a9131b877b3269
Bogdan <lebed4086@gmail.com>
2020-07-30 13:43:05 +0300

    update top widget

--- js/select_county.js
+++ js/select_county.js
@@ -1,10 +1,12 @@
 $(document).ready(() => {
     $('.container-widget-menu__elem2').click(() => {
         $('.country-submenu').toggleClass('active');
+        $('.container-widget-menu__elem2').toggleClass('active');
     });
     $(document).click((e) => {
-        if(e.target.className !== 'container-widget-menu__elem2'){
+        if(e.target.className !== 'container-widget-menu__elem2 active'){
             $('.country-submenu').removeClass('active');
+            $('.container-widget-menu__elem2').removeClass('active');
         }
     });
 });
--- js/select_lang.js
+++ js/select_lang.js
@@ -1,10 +1,12 @@
 $(document).ready(() => {
     $('.container-widget__lang').click(() => {
         $('.lang-submenu').toggleClass('active');
+        $('.container-widget__lang').toggleClass('active');
     });
     $(document).click((e) => {
-        if(e.target.className !== 'container-widget__lang'){
+        if(e.target.className !== 'container-widget__lang active'){
             $('.lang-submenu').removeClass('active');
+            $('.container-widget__lang').removeClass('active');
         }
     });
 });
